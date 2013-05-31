ff4a34f46b44b38e059909ea6b09dbe78625bf6c
July <dsm991213@gmail.com>
2013-05-31 20:56:36 +0800

    add_food_2
    a new XML learning,very happy!
    make a none-use TextView 's layout_weight = 1 then the linearlayout
    space will be used filly

---
+++ demo_add_food/src/ord/july/demo_add_food/MainActivity.java
@@ -0,0 +1,69 @@
+package ord.july.demo_add_food;
+
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.view.Menu;
+import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	private Button button1;
+	private Button button2;
+	private ImageView image1;
+	private TextView text1;
+	@Override 
+	protected void onActivityResult(int requestCode,int resultCode,Intent data) { 
+	//完成照相后回调用此方法 
+	super.onActivityResult(requestCode, resultCode, data); 
+	switch (resultCode) { 
+	case Activity.RESULT_OK: { 
+	Uri uri = data.getData(); 
+	Cursor cursor = this.getContentResolver().query(uri, null, 
+	null, null, null); 
+	cursor.moveToFirst(); 
+	cursor.close(); 
+	 //Options options = new BitmapFactory.Options(); 
+	//options.inJustDecodeBounds = false; 
+	 //options.inSampleSize = 10; 
+	// Bitmap bitmap = BitmapFactory.decodeFile(imgPath, options); 
+	image1.setImageURI(uri);
+	} 
+	break; 
+	case Activity.RESULT_CANCELED:// 取消 
+	break; 
+	} 
+	} 
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_main);
+		button1=(Button)findViewById(R.id.choosepic);
+		image1=(ImageView)findViewById(R.id.foodpic);
+		text1=(TextView)findViewById(R.id.add_foodname);
+button1.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent i = new Intent(Intent.ACTION_PICK, 
+						android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);//调用android的图库 
+						startActivityForResult(i, 2); 
+			
+
+			}
+		});
+	}
+
+
+
+}
