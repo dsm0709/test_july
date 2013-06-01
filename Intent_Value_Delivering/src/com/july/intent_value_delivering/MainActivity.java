b53e3e1a406cca15bc87f146adcd9660de444213
July <dsm991213@gmail.com>
2013-06-01 23:59:24 +0800

    Intent传值Demo_value_delivering1.1
    完善一些页面

--- Intent_Value_Delivering/src/com/july/intent_value_delivering/MainActivity.java
+++ Intent_Value_Delivering/src/com/july/intent_value_delivering/MainActivity.java
@@ -1,22 +1,37 @@
 package com.july.intent_value_delivering;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	
+	private Button btn1;
+	private Button btn2;
+	private Button btn3;
+	private Button btn4;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		btn1 = (Button)findViewById(R.id.button1);
+		btn2 = (Button)findViewById(R.id.button2);
+		btn3 = (Button)findViewById(R.id.button3);
+		btn4 = (Button)findViewById(R.id.button4);
+		
+		btn1.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, Open_otheractivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 
 }
